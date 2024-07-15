379ffc97c6cd457d3a5793cd0195c21d1da4021d
BacSiChongKhungBo <huywewe123@gmail.com>
2024-07-15 15:05:26 +0700

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,14 @@
+using DAL.DALversion2;
+
+
+namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Class1 c = new Class1();
+        }
+    }
+}
